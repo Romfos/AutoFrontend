7b40988d2dbf93aec01f0d8366d6b54b95e14900
Romfos <Romfos@tut.by>
2023-01-23 13:48:53 +0100

    Update AutoFrontend models

---
+++ src/AutoFrontend/Models/Argument.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace AutoFrontend.Models;
+
+public sealed class Argument
+{
+    public string Name { get; }
+    public Type ValueType { get; }
+
+    public Argument(string name, Type valueType)
+    {
+        Name = name;
+        ValueType = valueType;
+    }
+}
--- src/AutoFrontend/Models/Function.cs
+++ src/AutoFrontend/Models/Function.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace AutoFrontend.Models;
@@ -7,6 +8,9 @@ public sealed class Function
     public object? Target { get; }
     public MethodInfo MethodInfo { get; }
     public string Name { get; }
+
+    public List<Argument> Input { get; } = new();
+    public List<Argument> Output { get; } = new();
 
     public Function(object? target, MethodInfo methodInfo)
     {
