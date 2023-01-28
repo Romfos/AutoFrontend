6d9bf4f4d1d0c78ae9e410304f3ab4ee0a35943c
Romfos <Romfos@tut.by>
2023-01-28 16:09:29 +0100

    Update FunctionExecutor

--- src/AutoFrontend.Wpf/Services/FunctionExecutor.cs
+++ src/AutoFrontend.Wpf/Services/FunctionExecutor.cs
@@ -7,22 +7,22 @@ public sealed class FunctionExecutor
 {
     public async Task<object?> ExecuteFunctionAsync(Function function, object?[] parameters)
     {
+        var returnType = function.MethodInfo.ReturnType;
         var result = function.MethodInfo.Invoke(function.Target, parameters);
-        if (IsAsyncDataType(result, out var task))
+
+        if (returnType == typeof(void) || result == null)
         {
-            return await task;
+            return null;
+        }
+        if (returnType == typeof(Task) || returnType == typeof(ValueTask))
+        {
+            await (dynamic)result;
+            return null;
+        }
+        if (returnType.GetMethod(nameof(Task.GetAwaiter)) != null)
+        {
+            return await (dynamic)result;
         }
         return result;
-    }
-
-    private bool IsAsyncDataType(object? result, out dynamic? task)
-    {
-        if (result != null && result.GetType().GetMethod(nameof(Task.GetAwaiter)) != null)
-        {
-            task = result;
-            return true;
-        }
-        task = null;
-        return false;
     }
 }
