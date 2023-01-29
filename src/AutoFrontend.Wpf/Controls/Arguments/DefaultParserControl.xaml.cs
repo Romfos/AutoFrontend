b74c26072ecd74ad6667b56021ab5ce5881d7936
Romfos <Romfos@tut.by>
2023-01-29 02:33:48 +0100

    Added DefaultParserControl

--- src/AutoFrontend.Wpf/Controls/Arguments/DefaultParserControl.xaml.cs
+++ src/AutoFrontend.Wpf/Controls/Arguments/DefaultParserControl.xaml.cs
@@ -6,26 +6,35 @@ using System.Windows.Controls;
 
 namespace AutoFrontend.Wpf.Controls.Arguments;
 
-public partial class DefaultIntegerControl : UserControl, IArgumentControl
+public partial class DefaultParserControl : UserControl, IArgumentControl
 {
-    public DefaultIntegerControl()
+    public Func<string, object?>? TryParseFunction { get; set; }
+
+    private Argument? argument;
+
+    public DefaultParserControl()
     {
         InitializeComponent();
     }
 
     public void Configure(ServiceLocator serviceLocator, Argument argument, bool isReadOnly)
     {
+        this.argument = argument;
+
+        label.Content = $"Relevant {argument.AwaitResultType.FullName} is expected";
+
         textBox.IsReadOnly = isReadOnly;
         textBox.TextChanged += TextBox_TextChanged;
     }
 
     public object? GetArgumentValue()
     {
-        if (!int.TryParse(textBox.Text, out var interger))
+        var result = TryParseFunction?.Invoke(textBox.Text);
+        if (result == null)
         {
-            throw new Exception("Unable to parse number");
+            throw new Exception($"Unable to parse {argument?.AwaitResultType.FullName}");
         }
-        return interger;
+        return result;
     }
 
     public void SetArgumentValue(object? value)
@@ -35,7 +44,7 @@ public partial class DefaultIntegerControl : UserControl, IArgumentControl
 
     private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
     {
-        if (int.TryParse(textBox.Text, out _))
+        if (TryParseFunction?.Invoke(textBox.Text) != null)
         {
             label.Visibility = Visibility.Collapsed;
         }
--- src/AutoFrontend.Wpf/Services/ControlFactory.cs
+++ src/AutoFrontend.Wpf/Services/ControlFactory.cs
@@ -2,43 +2,60 @@ using AutoFrontend.Models;
 using AutoFrontend.Wpf.Controls.Arguments;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Controls;
 
 namespace AutoFrontend.Wpf.Services;
 
 public sealed class ControlFactory
 {
-    private readonly List<Component> components;
+    private readonly Dictionary<Type, Func<Control>> factories;
 
     public ControlFactory(List<Component> customComponents)
     {
-        components = GetDefaultComponents()
-            .Select(defaultComponent => customComponents.Find(x => x.ValueType == x.ValueType) ?? defaultComponent)
-            .ToList();
+        factories = CreateDefaultFactories();
+        OverrideFactories(factories, customComponents);
     }
 
     public Control? Create(Type valueType)
     {
-        var component = components.SingleOrDefault(x => x.ValueType == valueType);
-        if (component == null)
+        if (factories.TryGetValue(valueType, out var factory))
+        {
+            return factory();
+        }
+        else
         {
             return new DefaultArgumentControl();
         }
-        if (Activator.CreateInstance(component.ComponentType) is not Control control)
-        {
-            throw new Exception($"Component {component.ComponentType} should be wpf control");
-        }
-        if (control is not IArgumentControl)
-        {
-            throw new Exception($"Component {component.ComponentType} should implement {nameof(IArgumentControl)}");
-        }
-        return control;
     }
 
-    private IEnumerable<Component> GetDefaultComponents()
+    private Dictionary<Type, Func<Control>> CreateDefaultFactories()
     {
-        yield return new Component(typeof(DefaultStringControl), typeof(string));
-        yield return new Component(typeof(DefaultIntegerControl), typeof(int));
+        return new()
+        {
+            [typeof(string)] = () => new DefaultStringControl(),
+            [typeof(int)] = () => new DefaultParserControl()
+            {
+                TryParseFunction = x => int.TryParse(x, out var value) ? value : null
+            },
+        };
+    }
+
+    private void OverrideFactories(Dictionary<Type, Func<Control>> overriede, IEnumerable<Component> customComponents)
+    {
+        foreach (var customComponent in customComponents)
+        {
+            overriede[customComponent.ValueType] = () =>
+            {
+                if (Activator.CreateInstance(customComponent.ComponentType) is not Control control)
+                {
+                    throw new Exception($"Component {customComponent.ComponentType} should be wpf control");
+                }
+                if (control is not IArgumentControl)
+                {
+                    throw new Exception($"Component {customComponent.ComponentType} should implement {nameof(IArgumentControl)}");
+                }
+                return control;
+            };
+        }
     }
 }
