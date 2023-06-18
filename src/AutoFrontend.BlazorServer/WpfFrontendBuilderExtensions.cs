e98be1a1dca76ef778cfd2cf7f1e893e6d33c52a
Romfos <Romfos@tut.by>
2023-06-18 11:50:55 +0200

    Remove unusable code

--- src/AutoFrontend.BlazorServer/WpfFrontendBuilderExtensions.cs
+++ src/AutoFrontend.BlazorServer/WpfFrontendBuilderExtensions.cs
@@ -1,8 +1,6 @@
-using AutoFrontend.BlazorServer.Data;
 using AutoFrontend.Builders;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System.Reflection;
 
 namespace AutoFrontend.BlazorServer;
@@ -13,18 +11,11 @@ public static class BlazorServerFrontendBuilderExtensions
     {
         var builder = WebApplication.CreateBuilder();
 
-        // Add services to the container.
+        builder.Services.AddSingleton(frontendBuilder.ToFrontendModel());
         builder.Services.AddRazorPages().AddApplicationPart(Assembly.GetExecutingAssembly());
         builder.Services.AddServerSideBlazor();
-        builder.Services.AddSingleton<WeatherForecastService>();
 
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error");
-        }
 
         app.UseStaticFiles();
         app.UseRouting();
